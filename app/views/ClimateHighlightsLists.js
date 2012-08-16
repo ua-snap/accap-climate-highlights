e5cb716366862b374c2ac7022d0987f5f464e8e9
Bruce Crevensten <bruce.crevensten@gmail.com>
2012-08-15 18:38:19 -0800

    reduce some copy/paste code

--- app/views/ClimateHighlightsLists.js
+++ app/views/ClimateHighlightsLists.js
@@ -2,6 +2,26 @@ CH.views.ClimateHighlightsLists = Backbone.View.extend({
 
 	el: '#climate_highlights_lists',
 
+	// If any matching highlights meet the filter (daily/monthly), make an unordered list of
+	// ClimateHighlightListItems and append them to this element.
+	renderTitledFilteredList: function(filter, title) {
+		var interval = this.model.get('collection').filter(function(ch) {
+			return filter === ch.get('interval');
+		});
+
+		if(interval) {
+			$(this.el).append('<h3>'+title+'</h3>');
+			var ul = $('<ul>', {
+				'class':filter
+			});
+			_.each(interval, function(ch) {
+				v = new CH.views.ClimateHighlightListItem({model:ch});
+				ul.append( $(v.render().el) );
+			}, this);
+			$(this.el).append(ul);
+		}
+	},
+
 	render: function() {
 
 		$(this.el).empty();
@@ -11,36 +31,8 @@ CH.views.ClimateHighlightsLists = Backbone.View.extend({
 			throw("Model wasn't initialized correctly in ClimateHighlightLists section");
 		}
 
-		var daily = this.model.get('collection').filter(function(ch) {
-			return 'daily' === ch.get('interval');
-		});
+		this.renderTitledFilteredList('daily', 'Daily Events');
+		this.renderTitledFilteredList('monthly', 'Monthly Events');
 
-		if(daily) {
-			$(this.el).append('<h3>Daily Events</h3>');
-			var ul = $('<ul>', {
-				'class':'daily'
-			});
-			_.each(daily, function(ch) {
-				v = new CH.views.ClimateHighlightListItem({model:ch});
-				ul.append( $(v.render().el) );
-			}, this);
-			$(this.el).append(ul);
-		}
-
-		var monthly = this.model.get('collection').filter(function(ch) {
-			return 'monthly' === ch.get('interval');
-		});
-
-		if(monthly) {
-			$(this.el).append('<h3>Monthly Events</h3>');
-			var ul = $('<ul>', {
-				'class':'monthly'
-			});
-			_.each(monthly, function(ch) {
-				v = new CH.views.ClimateHighlightListItem({model:ch});
-				ul.append( $(v.render().el) );
-			}, this);
-			$(this.el).append(ul);
-		}
 	}
 });
