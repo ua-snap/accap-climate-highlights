5060e9fc10965bb43bfdf80f179c88ae0964b2e9
Bruce Crevensten <bruce.crevensten@gmail.com>
2012-08-17 11:19:34 -0800

    adds grunt config to package javascript

---
+++ grunt.js
@@ -0,0 +1,39 @@
+// This is the main application configuration file.  It is a Grunt
+// configuration file, which you can learn more about here:
+// https://github.com/cowboy/grunt/blob/master/docs/configuring.md
+//
+// initial reconfiguration for Climate Highlights project just needs to build a minified JS bundle.
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    // The clean task ensures all files are removed from the dist/ directory so
+    // that no files linger from previous builds.
+    clean: ["dist/", "build/"],
+
+    // Drupal version must omit jquery and bootstrap.
+    concat: {
+      drupal: {
+        src: [
+          'build/ch.js', // minified version of CH javascript
+          'assets/underscore-min.js',
+          'assets/backbone-min.js',
+          'assets/moment.min.js'
+          ],
+        dest: "dist/ch.js"
+      }
+    },
+
+    // Takes the built require.js file and minifies it for filesize benefits.
+    min: {
+      "build/ch.js": [
+        "app/**/*.js"
+      ]
+    }
+  }
+  );
+
+  // The release task will minifies then concats the files into dist/
+  grunt.registerTask("release", "min concat");
+
+};
